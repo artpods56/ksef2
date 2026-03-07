c894122ac019f23bbcf07a7d7dd4ae13b20401a1
Artur Podsiadły <artpods56@gmail.com>
2026-03-07 17:34:09 +0100

    test(mappers): fix authentication method mappings in unit tests

--- tests/unit/mappers/test_auth.py
+++ tests/unit/mappers/test_auth.py
@@ -76,8 +76,8 @@ class TestAuthResponseMapper:
 
         assert isinstance(output, domain_auth.AuthOperationStatus)
         assert output.start_date == mapped_input.startDate
-        assert output.authentication_method == from_spec(
-            mapped_input.authenticationMethod
+        assert (
+            output.authentication_method == mapped_input.authenticationMethodInfo.code
         )
         assert output.authentication_method_category == from_spec(
             mapped_input.authenticationMethodInfo.category
@@ -104,8 +104,8 @@ class TestAuthResponseMapper:
         assert isinstance(output, domain_auth.AuthenticationSession)
         assert output.reference_number == mapped_input.referenceNumber
         assert output.is_current == mapped_input.isCurrent
-        assert output.authentication_method == from_spec(
-            mapped_input.authenticationMethod
+        assert (
+            output.authentication_method == mapped_input.authenticationMethodInfo.code
         )
         assert output.authentication_method_category == from_spec(
             mapped_input.authenticationMethodInfo.category
